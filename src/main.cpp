53b4dc7e5c8e50637f665fa758dcdab7e3bb5fc0
Anis CHALI <anis.chali@exfo.com>
2023-08-05 00:15:46 -0400

    dpi: automatucly detect the device DPI and calculate a scale factor accordingly.

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,51 @@
 #include <QQmlApplicationEngine>
 #include <QSettings>
 #include <QQuickStyle>
+#include <QScreen>
+
 
 #include "app_environment.h"
 #include "import_qml_components_plugins.h"
 #include "import_qml_plugins.h"
 
+#define ORG_NAME "com.cciq.org"
+#define APP_NAME "QuranKarim"
+
+uint32_t get_dpi_scale_factor(void)
+{
+    QScreen *s = QGuiApplication::primaryScreen();
+    if (!s)
+        return 1;
+    float dpi = s->logicalDotsPerInch();
+    if (dpi == 96.0)
+        return 1;
+
+    return std::ceil((dpi + 0.5) / 96.0) + 1;
+}
+
 int main(int argc, char *argv[])
 {
+    QSettings settings(ORG_NAME, APP_NAME);
     int ret = 0;
+restart:
+    qputenv("QT_SCALE_FACTOR", settings.value("scale_factor").toByteArray());
     set_qt_environment();
-    QGuiApplication app(argc, argv);
-    QGuiApplication::setApplicationName("QuranKarim");
-    QGuiApplication::setOrganizationName("com.cciq.org");
+
+    QGuiApplication *app = new QGuiApplication(argc, argv);
+    QGuiApplication::setApplicationName(APP_NAME);
+    QGuiApplication::setOrganizationName(ORG_NAME);
 
 
-    QSettings settings;
+    if (settings.value("scale_factor").toString().isEmpty())
+    {
+        uint32_t factor = get_dpi_scale_factor();
+        settings.setValue(QString("scale_factor"), QString::fromStdString(std::to_string(factor)));
+        delete app;
+        goto restart;
+    }
+
+
+
     if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE") &&
         settings.value("style").toString().isEmpty()) {
         QString style;
@@ -40,7 +70,7 @@ int main(int argc, char *argv[])
     }
 
     const QString styleInSettings = settings.value("style").toString();
-    if (styleInSettings.isEmpty())
+        if (styleInSettings.isEmpty())
         settings.setValue(QString("style"), QQuickStyle::name());
 
     QStringList builtInStyles = { QString("CustomStyle"), QString("Basic"),
@@ -58,7 +88,7 @@ int main(int argc, char *argv[])
 
     const QUrl url(u"qrc:Main/main.qml"_qs);
     QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        &engine, &QQmlApplicationEngine::objectCreated, app,
         [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
@@ -74,7 +104,8 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    ret = app.exec();
+    ret = app->exec();
 
+    delete app;
     return ret;
 }
